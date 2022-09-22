a3dd8fff44452fb9811d5c414205f66b9fed81cb
Elena <Elena@106-01.info2>
2022-09-22 14:17:15 +0200

    Ejercicio 3 terminado

--- EjerciciosBasicos/src/ejercicio3/Ejercicio3.java
+++ EjerciciosBasicos/src/ejercicio3/Ejercicio3.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		int actual;
-		int nacimiento;
+		// Declaración de variables
+		int actual; // En actual guardaremos el año actual
+		int nacimiento; // En nacimiento guardaremos el año de nacimiento
+		int edad; // En edad guardaremos la edad resultante
 		
+		// Creamos el Scanner para leer del teclado
 		Scanner leer = new Scanner(System.in);
 		
+		// Le pedimos al usuario el año actual
 		System.out.println("Introduzca el año actual: ");
-		actual = leer.nextInt();
+		actual = leer.nextInt(); // leemos el año actual
 		
+		// Le pedimos al usuario el año de nacimiento
 		System.out.println("Introduzca el año de nacimiento: ");
-		nacimiento = leer.nextInt();
+		nacimiento = leer.nextInt(); // llemos el año de nacimiento
+		
+		// Calculamos la edad
+		edad = actual - nacimiento;
+		
+		// Le decimos al usuario la edad que tiene
+		System.out.print("Usted tiene " + edad +  " años.");
 	}
 
 }
